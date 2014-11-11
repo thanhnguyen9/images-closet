79068fb9e07c5237cbc3920e3930932e1331f693
thanhnguyen <thanhnguyen.ldt@gmail.com>
2014-11-11 16:43:10 -0600

    Add tubular

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,16 +2,31 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
+
 //= require jquery
 //= require bootstrap-sprockets
+//= require jquery.tubular.1.0
+//= require_tree
+
+
 $(document).ready(function(){
 
+    // $("#click-upload").on("click", function(event){
+    //  event.preventDefault();
+    //  event.stopPropagation();
+    //   $("#form").show("fast");
+    //
+    // });
 
-  // $("#click-upload").on("click", function(event){
-  //  event.preventDefault();
-  //  event.stopPropagation();
-  //   $("#form").show("fast");
-  //
-  // });
+  $("#sign-in-click").on("click", function(event){
+    event.preventDefault();
+    event.stopPropagation();
+    $("#sign-in-homepage").toggle();
+
+  $('#wrapper').tubular({videoId: '0Bmhjf0rKe8'});
+  })
+
+
+	var options = { videoId: 'e4Is32W-ppk', start: 3 };
+	$('#wrapper').tubular(options);
 });
