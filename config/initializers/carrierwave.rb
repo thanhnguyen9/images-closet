6b152fd1518359c180a2d453ea9102e905fe6df9
thanhnguyen <thanhnguyen.ldt@gmail.com>
2014-11-15 12:50:55 -0600

    Change region of endpoint

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',
     :aws_access_key_id      => ENV['AWS_KEY_ID'],
     :aws_secret_access_key  => ENV['AWS_SECRET'],
-    :region                 => 'us-west-2'
+    :region                 => 'us-west-1'
   }
   config.fog_directory = ENV['AWS_BUCKET']
 end
